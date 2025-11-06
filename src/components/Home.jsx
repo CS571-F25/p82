6b2eba4d2bbdfae06f1bf8938f817e475d2fa4da
s-asobol <s-asobol@lwsd.org>
2025-11-05 23:07:54 -0600

    initial home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,21 @@
-export default function Home (props){
-    return <div>
-        <h1>Home!</h1>
-    </div>
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "grid",
+        placeItems: "center",
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <div>
+        <h1>Artur Sobol</h1>
+        <p style={{ opacity: 0.8 }}>CS @ UW–Madison</p>
+        <p style={{ fontSize: 14, opacity: 0.7 }}>
+          Welcome to my personal website!
+        </p>
+      </div>
+    </main>
+  );
 }
